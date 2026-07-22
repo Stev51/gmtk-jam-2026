2a9a4b17e8c3320b50f9378a90754eea940e7629
Stev51 <dinosaurrampage51@gmail.com>
2026-07-22 16:18:29 -0500

    Map hover regions

--- scripts/map_hover_zone.gd
+++ scripts/map_hover_zone.gd
@@ -1,5 +1,5 @@
 @tool
-extends Node2D
+extends Area2D
 
 @export var outline_image: Texture2D
 @export var hover_image: Texture2D
@@ -9,21 +9,24 @@ extends Node2D
 @onready var hover_img_node = $HoverImg
 @onready var hover_sound_node = $HoverSound
 @onready var click_sound_node = $ClickSound
-	
+
+func update_textures() -> void:
+	if outline_image != null:
+		outline_img_node.texture = outline_image
+	if hover_image != null:
+		hover_img_node.texture = hover_image
 
 func _ready() -> void:
 	
-	outline_img_node.texture = outline_image
-	hover_img_node.texture = hover_image
+	update_textures()
 	
 	if not Engine.is_editor_hint():
-		outline_img_node.visible = false
+		outline_img_node.visible = true
 		hover_img_node.visible = false
 
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
-		outline_img_node.texture = outline_image
-		hover_img_node.texture = hover_image
+		update_textures()
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
