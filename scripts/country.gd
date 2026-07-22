e20ddbdf95d7a9d14544fc21f135fd7921581e62
Isaac Mango <bigmangohead@gmail.com>
2026-07-22 16:53:29 -0500

    Add enums for resource types

--- scripts/country.gd
+++ scripts/country.gd
@@ -1,1 +1,11 @@
 class_name Country extends Node
+
+# value is index in the resources array
+enum ResourceType {MONEY=0, URANIUM=1, NUKES=2, INFLUENCE=3}
+var resources = [0, 0, 0, 0]
+
+func set_resource(resource: ResourceType, new_value):
+	resources[resource] = new_value
+
+func edit_resource(resource: ResourceType, delta):
+	resources[resource] = resources[resource] + delta
