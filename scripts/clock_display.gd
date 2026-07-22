c854b6fc69d98ea7b58ad12649be186566258da3
Stev51 <dinosaurrampage51@gmail.com>
2026-07-22 17:39:07 -0500

    DoomsdayClock helper functions

--- scripts/clock_display.gd
+++ scripts/clock_display.gd
@@ -5,4 +5,4 @@ var MAX_DISPLAY_TIME = 60.0
 @onready var hand_sprite_node = $Hand
 
 func _process(delta: float) -> void:
-	hand_sprite_node.rotation_degrees = (DoomsdayClock.time_left / MAX_DISPLAY_TIME) * -360.0
+	hand_sprite_node.rotation_degrees = (DoomsdayClock.get_time_left() / MAX_DISPLAY_TIME) * -360.0
--- scripts/doomsday_clock.gd
+++ scripts/doomsday_clock.gd
@@ -1,1 +1,14 @@
 extends Timer
+
+func pause() -> void:
+	set_paused(true)
+
+func resume() -> void:
+	set_paused(false)
+
+func add_time(seconds: float) -> void:
+	var carryover = get_time_left()
+	start(seconds + carryover)
+
+func subtract_time(seconds: float) -> void:
+	add_time(-seconds)
