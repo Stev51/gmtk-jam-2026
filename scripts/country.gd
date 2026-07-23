1d8c3a72400a81dd03c177614b24d849f02017f3
Stev51 <dinosaurrampage51@gmail.com>
2026-07-23 11:14:52 -0500

    Country resources

--- scripts/country.gd
+++ scripts/country.gd
@@ -1,16 +1,22 @@
-class_name Country extends Node
+class_name Country extends Resource
 
 var money
 var uranium
 var nukes
 var influence
 
+@export var long_name = ""
+@export var short_name = ""
+
 @export var initial_money = 0
 @export var initial_uranium = 0
 @export var initial_nukes = 0
 @export var initial_influence = 0
 
-func reset_resources():
+func _ready() -> void:
+	reset_resources()
+
+func reset_resources() -> void:
 	money = initial_money
 	uranium = initial_uranium
 	nukes = initial_nukes
