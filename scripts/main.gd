f86f75ca02ae67fecc2a258b4b8950538423477f
Stev51 <dinosaurrampage51@gmail.com>
2026-07-24 16:25:17 -0500

    Space now unpauses too

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,8 +2,16 @@ extends Node2D
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		Globals.game_speed = Globals.GameSpeed.PAUSED
+		
+		if Globals.game_speed == Globals.GameSpeed.PAUSED:
+			Globals.game_speed = Globals.GameSpeed.NORMAL
+		else:
+			Globals.game_speed = Globals.GameSpeed.PAUSED
+		
 	elif event.is_action_pressed("x1_speed"):
 		Globals.game_speed = Globals.GameSpeed.NORMAL
 	elif event.is_action_pressed("x2_speed"):
 		Globals.game_speed = Globals.GameSpeed.FAST
+
+func _process(_delta: float) -> void:
+	print(Globals.game_speed)
