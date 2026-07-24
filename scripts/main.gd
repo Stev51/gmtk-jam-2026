761f49574043c28b02cefbf12f9436e53c38f5ef
Stev51 <dinosaurrampage51@gmail.com>
2026-07-24 16:22:57 -0500

    Time control keybinds

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		Globals.game_speed = Globals.GameSpeed.PAUSED
+	elif event.is_action_pressed("x1_speed"):
+		Globals.game_speed = Globals.GameSpeed.NORMAL
+	elif event.is_action_pressed("x2_speed"):
+		Globals.game_speed = Globals.GameSpeed.FAST
