3c1af466929cc92a267fe918ba2175538733644d
tjones <trent@trentjones.com>
2026-07-24 21:55:12 -0400

    Passive Delta Updates and Example Actions
    Passive updates in country_manager.gd, example actions in country.gd

--- scripts/country.gd
+++ scripts/country.gd
@@ -4,6 +4,17 @@ var money
 var uranium
 var nukes
 var influence
+var labs
+var mines
+var research_progress
+var willingness_to_fire_nukes
+var signed_armistice
+
+var delta_money
+var delta_uranium
+var delta_influence
+var delta_research_progress
+var delta_willingness_to_fire_nukes
 
 @export var long_name = ""
 @export var short_name = ""
@@ -11,7 +22,18 @@ var influence
 @export var initial_money = 0.0
 @export var initial_uranium = 0.0
 @export var initial_nukes = 0.0
-@export var initial_influence = 0.0
+@export var initial_influence = [0.0, 0.0, 0.0, 0.0, 0.0]
+@export var initial_labs = 0
+@export var initial_mines = 0
+@export var initial_research_progress = 0.0
+@export var initial_willingness_to_fire_nukes = 0.0
+@export var initial_signed_armistice = false
+
+@export var initial_delta_money = 0.0
+@export var initial_delta_uranium = 0.0
+@export var initial_delta_influence = [0.0, 0.0, 0.0, 0.0, 0.0]
+@export var initial_delta_research_progress = 0.0
+@export var initial_delta_willingness_to_fire_nukes = 0.0
 
 func init() -> void:
 	reset_resources()
@@ -21,3 +43,30 @@ func reset_resources() -> void:
 	uranium = initial_uranium
 	nukes = initial_nukes
 	influence = initial_influence
+	labs = initial_labs
+	mines = initial_mines
+	research_progress = initial_research_progress
+	willingness_to_fire_nukes = initial_willingness_to_fire_nukes
+	signed_armistice = initial_signed_armistice
+	delta_money = initial_delta_money
+	delta_uranium = initial_delta_uranium
+	delta_influence = initial_delta_influence
+	delta_research_progress = initial_delta_research_progress
+	delta_willingness_to_fire_nukes = initial_delta_willingness_to_fire_nukes
+
+func raise_war_taxes() -> void:
+	if influence >= 100:
+		influence -= 100
+		delta_money += 10
+
+#function called for spreading positive propaganda of own country
+func spread_propaganda(target):
+	if money >= 100:
+		money -= 100
+		delta_influence[target] += 10
+
+#function called for spreading negative propaganda of target country
+func spread_counter_propaganda(target):
+	if money >= 100:
+		money -= 100
+		CountryManager.countries[target].delta_influence[target] -= 10
--- scripts/country_manager.gd
+++ scripts/country_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var timed_out_flag = false
 
 var NUKES_TO_INFLUENCE = 0.05
-var INFLUENCE_SCARE_DECREASE = 10.0
+var INFLUENCE_WILLINGNESS_DECREASE = 10.0
 var INFLUENCE_TO_MONEY = 1.0
 var MONEY_TO_URANIUM = 132000.0
 var MONEY_TO_NUKES = 500.0
@@ -28,43 +28,58 @@ func _process(delta: float) -> void:
 			delta *= 2
 		
 		for country in countries:
-			process_country(country, get_total_influence(), delta)
+			process_country(country, delta)
 
-func get_total_influence() -> float:
-	var count = 0.0
-	for country in countries:
-		count += country.influence
-	return count
+func process_country(country: Country, delta: float) -> void:
+	#Five passive stats: money, uranium, research, willingness, influence
+	#delta uranium and delta progress should never be negative. Money can go negative lol. Willingness and influence are bounded at zero.
+	country.money += country.delta_money * delta
+	country.uranium += country.delta_uranium * delta
+	country.research_progress += country.delta_research_progress * delta
+	country.willingness_to_fire_nukes += country.delta_willingness_to_fire_nukes * delta
+	if country.willingness_to_fire_nukes < 0.0:
+		country.willingness_to_fire_nukes = 0.0
+	for country_b in countries:
+		country.influence[country_b] += country.delta_influence[country_b]
+		if country.influence[country_b] < 0.0:
+			country.influence[country_b] = 0.0
+	#Add math to calculate delta for willingness
 
-func get_highest_influence() -> float:
-	var highest = 0.0
-	for country in countries:
-		if country.influence > highest:
-			highest = country.influence
-	return highest
+#func get_total_influence() -> float:
+	#var count = 0.0
+	#for country in countries:
+		#count += country.influence
+	#return count
+
+#func get_highest_influence() -> float:
+	#var highest = 0.0
+	#for country in countries:
+		#if country.influence > highest:
+			#highest = country.influence
+	#return highest
 
 # All of this process is just for demonstration
-func process_country(country: Country, total_influence: float, delta: float) -> void:
-	
-	country.influence += country.nukes * NUKES_TO_INFLUENCE * delta
-	country.influence -= ((total_influence - country.influence) / total_influence ) * INFLUENCE_SCARE_DECREASE * delta
-	if country.influence < 0.0:
-		country.influence = 0.0
-	
-	country.money += country.influence * INFLUENCE_TO_MONEY * delta
-	
-	if country.uranium >= URANIUM_TO_NUKES or country.money >= MONEY_TO_NUKES:
-		
-		while country.uranium >= URANIUM_TO_NUKES and country.money >= MONEY_TO_NUKES:
-			country.nukes += 1.0
-			country.uranium -= URANIUM_TO_NUKES
-			country.money -= MONEY_TO_NUKES
-	
-	else:
-		
-		while country.money >= MONEY_TO_URANIUM:
-			country.uranium += 1.0
-			country.money -= MONEY_TO_URANIUM
+#func process_country(country: Country, total_influence: float, delta: float) -> void:
+	#
+	#country.influence += country.nukes * NUKES_TO_INFLUENCE * delta
+	#country.influence -= ((total_influence - country.influence) / total_influence ) * INFLUENCE_WILLINGNESS_DECREASE * delta
+	#if country.influence < 0.0:
+		#country.influence = 0.0
+	#
+	#country.money += country.influence * INFLUENCE_TO_MONEY * delta
+	#
+	#if country.uranium >= URANIUM_TO_NUKES or country.money >= MONEY_TO_NUKES:
+		#
+		#while country.uranium >= URANIUM_TO_NUKES and country.money >= MONEY_TO_NUKES:
+			#country.nukes += 1.0
+			#country.uranium -= URANIUM_TO_NUKES
+			#country.money -= MONEY_TO_NUKES
+	#
+	#else:
+		#
+		#while country.money >= MONEY_TO_URANIUM:
+			#country.uranium += 1.0
+			#country.money -= MONEY_TO_URANIUM
 
 func zero_hour() -> void:
 	timed_out_flag = true
