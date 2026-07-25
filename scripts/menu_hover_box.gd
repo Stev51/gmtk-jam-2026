3e76da8502c1d48e6083b88809ba44cb42af4546
Stev51 <dinosaurrampage51@gmail.com>
2026-07-25 11:28:38 -0500

    Menu hover text finished yay

--- scripts/menu_hover_box.gd
+++ scripts/menu_hover_box.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var background_node = $Background
 @onready var title_node = $TitleTextBox
 @onready var description_node = $DescriptionTextBox
 
@@ -15,15 +16,18 @@ func _input(event) -> void:
 func hover(title: String, desc: String) -> void:
 	hovered += 1
 	set_text(title, desc)
+	set_boundaries.call_deferred()
 	show()
-	print(hovered)
 
 func unhover() -> void:
 	hovered -= 1
 	if hovered <= 0:
 		hide()
-	print(hovered)
 
 func set_text(title: String, desc: String) -> void:
 	title_node.text = title
 	description_node.text = desc
+
+func set_boundaries() -> void:
+	background_node.size.x = max(title_node.size.x, description_node.size.x) + 20
+	background_node.size.y = title_node.size.y + description_node.size.y + 10
