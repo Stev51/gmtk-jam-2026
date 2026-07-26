7d6606c1b5496d792d387189cdc57d41e7f207de
Stev51 <dinosaurrampage51@gmail.com>
2026-07-26 10:14:30 -0500

    Delta displays

--- scripts/resource_displays.gd
+++ scripts/resource_displays.gd
@@ -8,7 +8,11 @@ extends Control
 @onready var mines_node = $GridContainer/Mines
 @onready var influence_node = $GridContainer/Influence
 
+@onready var influence_delta_node = $GridContainer/Influence/DeltaLabel
+@onready var money_delta_node = $GridContainer/Money/DeltaLabel
+
 func _process(_delta: float) -> void:
+	
 		money_node.set_text("$" + str(snapped(Globals.PlayerCountry.money, 0.01)))
 		uranium_node.set_text(str(snapped(Globals.PlayerCountry.uranium, 0.01)))
 		nukes_node.set_text(str(int(Globals.PlayerCountry.nukes)))
@@ -16,3 +20,6 @@ func _process(_delta: float) -> void:
 		labs_node.set_text(str(int(Globals.PlayerCountry.labs)))
 		mines_node.set_text(str(int(Globals.PlayerCountry.mines)))
 		influence_node.set_text(str(snapped(Globals.PlayerCountry.influence[Globals.PlayerCountry.index], 0.01)))
+		
+		influence_delta_node.text = "[i]+ " + str(snapped(Globals.PlayerCountry.delta_influence[Globals.PlayerCountry.index] * CountryManager.DELTA_MOD, 0.001)) + " / s"
+		money_delta_node.text = "[i]+ " + str(snapped(Globals.PlayerCountry.delta_money * CountryManager.DELTA_MOD, 0.001)) + " / s"
