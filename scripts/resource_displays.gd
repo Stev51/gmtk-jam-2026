dcd842299399b9c4ebcd10f5dd91dd9044ad9069
Stev51 <dinosaurrampage51@gmail.com>
2026-07-25 23:01:05 -0500

    Influence display

--- scripts/resource_displays.gd
+++ scripts/resource_displays.gd
@@ -6,11 +6,13 @@ extends Control
 @onready var research_node = $GridContainer/Research
 @onready var labs_node = $GridContainer/Labs
 @onready var mines_node = $GridContainer/Mines
+@onready var influence_node = $GridContainer/Influence
 
 func _process(_delta: float) -> void:
-		money_node.set_text(str(int(Globals.PlayerCountry.money)))
+		money_node.set_text("$" + str(int(Globals.PlayerCountry.money)))
 		uranium_node.set_text(str(int(Globals.PlayerCountry.uranium)))
 		nukes_node.set_text(str(int(Globals.PlayerCountry.nukes)))
 		research_node.set_text(str(int(Globals.PlayerCountry.research_progress)))
 		labs_node.set_text(str(int(Globals.PlayerCountry.labs)))
 		mines_node.set_text(str(int(Globals.PlayerCountry.mines)))
+		influence_node.set_text(str(Globals.PlayerCountry.influence[Globals.PlayerCountry.index]))
